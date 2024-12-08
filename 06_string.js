a16c6a72b5a94424df125c0963911516de93c9bb
Sahil-khola <blackiayaduvanshia@gmail.com>
2024-12-08 15:33:31 +0530

    JAVASCRIPT

---
+++ 06_string.js
@@ -0,0 +1,33 @@
+// STRING >
+
+const name = "Sahil "
+const repoCount = 5;
+const gameName = new String("Sahil-khola")
+
+// console.log(name + repoCount + " value")
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+// console.log(name[0])
+// console.log(name[1])
+// console.log(name[2])
+// console.log(name[3])
+// console.log(name[4])
+// console.log(name[5])
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.charAt(1))
+// console.log(name.indexOf("h"))
+// console.log(gameName.substring(0,3));
+
+// const anotherString = gameName.slice(0,4)
+// console.log(anotherString)
+
+// const newStringOne = "   khola   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://khola.com/3974"
+// console.log(url.replace("3974","7202"))
+// console.log(url.includes("khola"))
+
+console.log(gameName.split("-"));
+
