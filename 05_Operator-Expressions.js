33807b49dfe442e89143728138da9406ae6f5168
Sahil-khola <blackiayaduvanshia@gmail.com>
2024-12-19 22:31:14 +0530

    JAVASCRIPT

---
+++ 05_Operator-Expressions.js
@@ -0,0 +1,49 @@
+// operator in JS
+//  Arthmetic operator 
+// let a = 10;
+// let b = 4;
+// console.log("a+b = ",a+b);
+// console.log("a-b = ",a-b);
+// console.log("a/b = ",a/b);
+// console.log("a*b = ",a*b);
+// console.log("a*b = ",a**b);
+// console.log("a%b = ",a%b);
+// console.log("a++ = ",a++);
+// console.log(a);
+// console.log("a-- = ",a--);
+// console.log(a);
+
+//Assignment operator
+// let a = 10;
+// a+= 1 // a= 10+1
+// console.log(a);
+// a-= 1
+// console.log(a);
+// a*= 1
+// console.log(a);
+// a/= 2
+// console.log(a);
+
+//Comparison operator
+// let a1 = 7;
+// let a2 = "7";
+// console.log('a1==a2',a1==a2);
+// console.log('a1==a2',a1!=a2);
+// console.log('a1===a2',a1===a2);
+// console.log('a1!==a2',a1!==a2);
+
+
+//logical operator
+// && logical and
+// || logical or
+// ! logical not 
+
+let x = 5;
+let y = 6;
+// console.log(x<y && x==5); both are true
+console.log(x<y || x==6); // any one is true it will be true
+// console.log(!false)
+
+
+// 7 + 8 = 15  where + is operator and 7,8 is oprendes 15 is result
+
